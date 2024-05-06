6ecbf829e0fb780f95046d5db23158ecc67724e2
Adnanh4141 <adnanh4141@gmail.com>
2024-05-06 10:42:54 +0600

    final production

--- index.js
+++ index.js
@@ -9,19 +9,12 @@ require("dotenv").config();
 
 
 
-
-// todoSchema.query = {
-//     byLanguage: function (language){
-//         return this.find({title:new regExp(language,"i")});//new RegExp
-//        res.status(200).json({
-//         data,
-//        })
-//     }
-// }
-
 const port = process.env.PORT || 3000;
+
  app.listen( port, ()=> {
-   const dbURL = "mongodb://localhost:27017/QuizManagement";
+   const dbURL = process.env.DB_URL || "mongodb://localhost:27017/QuizManagement";
+  
+
    console.log(`Server is up and running on http://localhost:${port}`)
    connectDB(dbURL)
 });
