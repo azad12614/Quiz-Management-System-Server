b612cacd663ec0fe211ba66cd502267a4db9a3c9
Adnanh4141 <adnanh4141@gmail.com>
2024-05-06 11:54:03 +0600

    e

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 
+const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+connectDB(dbURL)
 
 const port = process.env.PORT || 3000;
 
  app.listen( port, ()=> {
   //  const dbURL = process.env.DB_URL|| "mongodb://localhost:27017/QuizManagement";
-   const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
    console.log(`Server is up and running on http://localhost:${port}`)
-   connectDB(dbURL)
+  
 });
