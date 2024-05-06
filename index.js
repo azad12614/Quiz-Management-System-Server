b1f696db9adb8c6c4b73e6775f9f933dffe7f1c5
Adnanh4141 <adnanh4141@gmail.com>
2024-05-06 11:51:04 +0600

    mongoose

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 
  app.listen( port, ()=> {
-   const dbURL = process.env.DB_URL|| "mongodb://localhost:27017/QuizManagement";
-  //  const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  //  const dbURL = process.env.DB_URL|| "mongodb://localhost:27017/QuizManagement";
+   const dbURL = `mongodb+srv://Adnan:LPL1MheB0B4bsCvg@cluster0.mpobb6h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
    console.log(`Server is up and running on http://localhost:${port}`)
    connectDB(dbURL)
